0c5ca7cd83b704cd1c5d676f5ea2e4878f555a1c
EA239720 <75696001+EA239720@users.noreply.github.com>
2024-02-10 14:41:38 -0500

    Cambio de menu
    Se añadio un observador para que el menu de navegacion cambie de color en cuando cambie su propiedad sticky

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,33 @@
+import { useEffect, useRef, useState } from 'react';
+
 import styles from '../styles/Menu.module.scss';
 
 function Menu({ setP }) {
+    const [isSticky, setIsSticky] = useState(false);
+    const sticky = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+                setIsSticky(entry.intersectionRatio < 1);
+            },
+            { threshold: [1] }
+        );
+
+        console.log(isSticky);
+        
+        if(sticky.current) {
+            observer.observe(sticky.current);
+        }
+
+        return () => {
+            if(sticky.current) {
+                observer.unobserve(sticky.current);
+            }
+        }
+    }, []);
+
     return (
-        <div className={styles.body}>
+        <div ref={sticky} className={styles.body} stik={isSticky ? 1 : 0}>
             <div className={styles.menu}>
                 <a href={'#'} onClick={() => setP(1)}>INICIO</a>
                 <a href={'#'} onClick={() => setP(2)}>HISTORIA</a>
