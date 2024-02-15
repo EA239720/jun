b815cb36707b15841354774f011322a9e5e91f78
EA239720 <75696001+EA239720@users.noreply.github.com>
2024-02-15 13:47:37 -0600

    Se implementa filtro a los dojos
    Por medio del dato del origen de cada dojo, se implementan botones para poder mostrar los dojos filtrados por el orgen.

--- src/components/Dojos.js
+++ src/components/Dojos.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Card from './subcomponents/Card';
 
 import styles from '../styles/Dojos.module.scss';
@@ -5,23 +7,33 @@ import styles from '../styles/Dojos.module.scss';
 import Data from '../Data/Dojos.json';
 
 export default function Dojos({ setP }) {
+    const [origen, setOrigen] = useState('');
+
     return (
         <div className={styles.body}>
             <div className={styles.cellButton}>
                 <button onClick={() => setP(3)}> <p> FILOSOFIA </p> </button>
                 <button onClick={() => setP(1)}> <p> INICIO </p> </button>
             </div>
+            <div className={styles.buttons}>
+                <button onClick={() => setOrigen('MEXICO')}>México</button>
+                <button onClick={() => setOrigen('USA')}>USA</button>
+                <button onClick={() => setOrigen('HONDURAS')}>Honduras</button>
+            </div>
+            <div className={styles.grid}>
             {
                 Data.map(d => (
-                    <Card
-                        key={d.dojo}
-                        dojo={d.dojo}
-                        sensei={d.sensei}
-                        ciudad={d.ubicacion}
-                        integrantes={d.integrantes}
-                    />
+                    d.origen === origen ?
+                        <Card
+                            key={d.dojo}
+                            dojo={d.dojo}
+                            sensei={d.sensei}
+                            ciudad={d.ubicacion}
+                            integrantes={d.integrantes}
+                        /> : null
                 ))
             }
+            </div>
         </div>
     );
 }
