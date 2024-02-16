6fc6ab09a383ae48a0a17e4c77b11dee91b01e83
EA239720 <75696001+EA239720@users.noreply.github.com>
2024-02-15 18:38:52 -0600

    Cambio a la pagina Home
    Pequeño cambio para mejorar la presentacion y el aspecto responsivo de la pagina de inicio.

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,12 @@ import { MdArrowForwardIos, MdArrowBackIosNew } from 'react-icons/md';
 export default function Home({ setP }) {
     return (
         <div className={styles.body}>
-            <div className={styles.cellButton}>
-                <button onClick={ () => setP(4) } > <p> DOJOS </p> </button>
-                <button onClick={ () => setP(2) } > <p> HISTORIA </p> </button>
+           <div className={styles.subBody}>
+                <div className={styles.legend}>
+                    <h1 /* className={styles.about} */>La escuela de <i>KARATE DO JUNSEIKHAN</i> es una asociación que busca regresar a las raíces del arte marcial. No buscamos solamente ganar medallas en torneos, sino también cultivar en nuestras vidas la filosofía de vida del KARATE DO.</h1>
+                </div>
+                <img src={Logo} className={styles.logo} alt={'logo'} />
             </div>
-            <img src={Logo} className={styles.logo} alt={'logo'} />
-            <h1 className={styles.about}>La asociacion de KARATE DO JUNSEIKHAN es una asociacion sin fines de lucro, que busca regresar a las raices del arte marcial. Asi, no buscamos solamente ganar medallas en torneos, sino tambien cultivar en nuestras vidas la filosofia de vida del KARATE DO.</h1>
         </div>
     );
 }
