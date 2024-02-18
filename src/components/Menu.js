38cc33be1bd286005a03929ee59098894b8b4e0a
EA239720 <75696001+EA239720@users.noreply.github.com>
2024-02-18 16:09:58 -0600

    Cambio de menu
    Se aplico un cambio al menu de la pagina para que funcionara mejor de forma responsiva.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,8 +2,15 @@ import { useEffect, useRef, useState } from 'react';
 
 import styles from '../styles/Menu.module.scss';
 
+import menuButton from '../Assets/line-md--close-to-menu-transition.svg';
+import closeButton from '../Assets/line-md--menu-to-close-transition.svg';
+import menuIcon from '../Assets/line-md--menu.svg';
+import header from '../Assets/DOYO DORADO.png';
+
 function Menu({ setP }) {
     const [isSticky, setIsSticky] = useState(false);
+    const [menu, setMenu] = useState(0);
+
     const sticky = useRef();
 
     useEffect(() => {
@@ -13,8 +20,6 @@ function Menu({ setP }) {
             { threshold: [1] }
         );
 
-        console.log(isSticky);
-        
         if(sticky.current) {
             observer.observe(sticky.current);
         }
@@ -27,8 +32,10 @@ function Menu({ setP }) {
     }, []);
 
     return (
-        <div ref={sticky} className={styles.body} stik={isSticky ? 1 : 0}>
-            <div className={styles.menu}>
+        <div ref={sticky} className={styles.body} stik={isSticky ? 1 : 0} men={menu}>
+            <button className={styles.menuButton} onClick={() => setMenu(menu === 1 ? 2 : 1)}><img src={menu === 0 ? menuIcon : menu === 1 ? closeButton : menuButton} alt='menu' /></button>
+            <div className={styles.menu>
+                <img src={header} alt='head' className={styles.header} />
                 <a href={'#'} onClick={() => setP(1)}>INICIO</a>
                 <a href={'#'} onClick={() => setP(2)}>HISTORIA</a>
                 <a href={'#'} onClick={() => setP(3)}>FILOSOFIA</a>
