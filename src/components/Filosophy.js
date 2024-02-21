9330c99a079d053428ed697160270ed576297347
EA239720 <75696001+EA239720@users.noreply.github.com>
2024-02-20 18:01:10 -0600

    Presentacion uno
    Cambios hechos para presentar el continido de ciertas paginas.

--- src/components/Filosophy.js
+++ src/components/Filosophy.js
@@ -2,7 +2,7 @@ import styles from '../styles/Filosophy.module.scss';
 
 export default function Filosophy({ setP }) {
     return(
-        <div className={styles.body}>
+        <div className={styles.subBody}>
             <p>Las artes marciales hoy en dia, incluyendo el KARATE DO, tienen dos vertientes practicas, la vertiente deportiva y la vertiente filosofica.</p>
             <p>La vertiente deportiva de las artes marciales, es aquella que enfoca el entrenamiento hacia el exterior, la que se centra en el ejercicio y las competencias, generando practicantes cuya maxima aspiracion es recibir elogios y felicitaciones de las personas que los rodean, sus maestros, familiares, amigos y compañeros, y cuyo logro mas grande es ser la persona, dentro de una escuela u organizacion, que mas trofeos y medallas tiene en su haber.</p>
             <p>Por su lado la vertiente filosofica, manejada en japón como BUDO, es aquella que enfoca el entrenamiento de un arte marcial hacia el interior de la persona, no solo convirtiendola en practicante de un arte marcial, sino transformando el arte marcial, en si mismo, en una forma de vida, generando personas con altos valores morales y eticos que no solo son aplicables dentro del dojo con su sensei y sus compañeros, sino tambien fuera de él y con todas las personas con las que convive.</p>
